d5876ef4d275f8e6b544bc833e8c537c8f6d6c83
Pat Cavit <pcavit@gmail.com>
2013-01-11 13:07:55 -0800

    before/afterEach example re-added
    As a test this time, instead of globally.

--- test/simple.js
+++ test/simple.js
@@ -6,12 +6,42 @@
 describe("test thing", function() {
     describe("smaller test thing", function() {
         it("does a test", function(done) {
-            casper.start("http://google.com", function() {
-                expect(this.currentHTTPStatus).to.equal(301);
+            casper.start("https://www.google.com", function() {
+                expect(this.currentHTTPStatus).to.equal(200);
             });
             
             casper.run(function() {
                 done();
+            });
+        });
+    });
+    
+    describe("before/afterEach example", function() {
+        beforeEach(function() {
+            casper.start();
+        });
+        
+        afterEach(function(done) {
+            if(!casper.steps.length) {
+                return done();
+            }
+            
+            casper.run(function() { done(); });
+        });
+        
+        it("does a simple test", function() {
+            casper.open("https://www.google.com");
+             
+            casper.then(function() {
+                expect(this.currentHTTPStatus).to.equal(200);
+            });
+        });
+        
+        it("should fail", function() {
+            casper.open("https://www.google.com");
+            
+            casper.then(function() {
+                expect(this.currentHTTPStatus).to.equal(500);
             });
         });
     });
